c3f7db036ae8fae93693ad076ea506b900ee8614
StudentOne1 <punchedjesuschrist@gmail.com>
2015-05-20 15:42:54 +0100

    Update register.blade.php

--- app/views/account/register.blade.php
+++ app/views/account/register.blade.php
@@ -30,10 +30,47 @@
 				<td>{{ Form::email('email', Input::old('email'), ['class'=>'form-control']) }}</td>
 			</tr>
 			<tr>
-				<td></td>
+				<td>{{ Form::hidden('e_lat', '', ['id'=>'e_lat']) }}</td>
+				<td>{{ Form::hidden('e_lng', '', ['id'=>'e_lng']) }}</td>
 				<td>{{ Form::submit('Register account', ['class'=>'btn btn-block btn-primary']) }}</td>
 			</tr>
 		</table>
+
+
+		<script language="javascript" type="text/javascript">
+
+
+			var e_lng = document.getElementById("e_lng");
+			var e_lat = document.getElementById("e_lat");
+
+
+			function getLocation() 
+			{
+
+			    if (navigator.geolocation)
+			    {
+
+			        navigator.geolocation.getCurrentPosition(setPosition);
+
+			    } 
+			    else 
+			    {
+			        alert("Geolocation is not supported by this browser.");
+			    }
+
+			}
+
+
+			function setPosition(position)
+			{
+			    e_lat.value = position.coords.latitude;
+			    e_lng.value = position.coords.longitude; 
+			}
+
+
+		</script>
+		<script type="text/javascript">getLocation();</script>
+
 
 	{{ Form::close() }}
 <!-- Register Field Ends-->
