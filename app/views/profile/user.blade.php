2e67484103e8810ea7b69d33ed6e16c9239983f0
StudentOne1 <punchedjesuschrist@gmail.com>
2015-05-22 18:04:17 +0100

    Update user.blade.php

--- app/views/profile/user.blade.php
+++ app/views/profile/user.blade.php
@@ -1,42 +1,114 @@
 @extends('templates.v2')
 
+
 @section('content')
-<h2>{{ ucfirst($title) }}</h2>
-<hr>
+
 
 @include('common.message')
+<img style="width: 200px; height: 200px; float:left;" src="{{{$profile->image}}}" class="thumbnail"/> 
+<h2 style="float:right;">{{ ucfirst($title) }}</h2>
 
-<!-- User Profile Area -->
-<table class="table .table-hover">
-	<tr>
-		<td colspan="2" align="center">
-			<img style="width: 200px; height: 200px;" src="{{{$profile->image}}}" class="thumbnail"/>
-		</td>
-	</tr>
-	<tr>
-		<td>Profile Name: </td>
-		<td>{{ e($profile->profile_name) }}</td>	
-	</tr>
-	<tr>
-		<td>Description: </td>
-		<td>{{ e($profile->description) }}</td>
-	</tr>
-	<tr>
-		<td>Created at: </td>
-		<td>{{ e($profile->created_at) }}</td>
-	</tr>
-	<tr>
-		<td>Updated at: </td>
-		<td>{{ e($profile->updated_at) }}</td>
-	</tr>
-	<tr>
-		<!-- If Logged -->
-		@if (Auth::check() && Auth::user()->id == $user_id)
-		<td colspan="2"><a href="{{ e(URL::route('profile-edit')) }}" class="btn btn-primary"/>Edit Profile</a></td>
-		@endif
-	</tr>
-</table>
-<!-- User Profile Area Ends -->
 
-<hr>
+@if (Auth::check() && Auth::user()->id == $user_id)
+<a href="{{ e(URL::route('profile-edit')) }}" class="btn btn-primary" style="float:right; margin-right:16px;" />Edit Profile</a>
+@else
+<a href="{{ e(URL::route('friend-request', array('id' => $user_id))) }}" style="float:right; margin-right:16px;">Befriend</a>
+@endif
+
+
+<div class="table-responsive col-md-4" style="width:100%;float:left;">
+
+
+@if (count($hosted_events) > 0)
+<div class="table-responsive col-md-4">
+	<table class="table table-hover.table-condensed">
+		<thead>
+			<tr>
+				<th>Event(s) Hosted:</th>
+			</tr>
+		</thead>
+
+		<tbody>
+
+			@foreach($hosted_events as $he)
+			<tr>
+				<td>
+					<a href="../event/{{ $he->id }}">{{ $he->e_name }} ({{ count(json_decode($he['attendees'], true)) }})</a>
+				</td>
+			</tr>
+			@endforeach
+
+		</tbody>
+	</table>
+</div>
+@endif
+
+
+@if (count($joined_events) > 0)
+<div class="table-responsive col-md-4">
+	<table class="table table-hover.table-condensed">
+		<thead>
+			<tr>
+
+				<th>Event(s) Joined:</th>
+				@if (Auth::check() && Auth::user()->id == $user_id)
+					<th>Leave event:</th>
+				@endif
+
+			</tr>
+		</thead>
+
+		<tbody>
+
+			@foreach($joined_events as $je)
+			<tr>
+
+				<td><a href="../event/{{ $je->event->first()->id }}">{{ e($je->event->first()->e_name) }}</a></td>
+
+				@if (Auth::check() && Auth::user()->id == $user_id)
+					<td>{{ link_to_action('invite-remove', 'Leave', array('user' => $je->host_id, 'event' => $je->event_id )) }}</td>			
+				@endif
+
+			</tr>
+			@endforeach
+
+		</tbody>
+	</table>
+</div>
+@endif
+
+
+@if (count($friends) > 0)
+<div class="table-responsive col-md-4">
+	<table class="table table-hover.table-condensed">
+		<thead>
+			<tr>
+
+				<th>Your Friend(s):</th>
+				@if (Auth::check() && Auth::user()->id == $user_id)
+					<th>Remove Friend</th>
+				@endif
+
+			</tr>
+		</thead>
+
+		<tbody>
+
+			@foreach($friends as $fr)
+			<tr>
+				<td><a href="{{ $fr->username }}">{{  $fr->username }}</a></td>
+
+				@if (Auth::check() && Auth::user()->id == $user_id)
+					<td>{{ link_to_action('friend-remove', 'Remove', array('id' => $fr->id)) }}</td>
+				@endif
+			</tr>
+			@endforeach
+
+		</tbody>
+	</table>
+</div>
+@endif
+
+</div>
+
 @stop
